12425c9ad0185638a77ab9435e3c1dc22bf82ae9
vikasjkitawat <vikasjkitawat@gmail.com>
2018-03-25 01:20:55 +0530

    Test Project and Commons Project added

---
+++ ContinuousIntegrationDemo.Commons/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinuousIntegrationDemo.Commons
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ ContinuousIntegrationDemo.Tests/CommonsTests/CalculatorTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ContinuousIntegrationDemo.Commons;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContinuousIntegrationDemo.Tests.CommonsTests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            var addCalculator = new Calculator();
+            var res = addCalculator.Add(20, 10);
+            Assert.AreEqual(res, 30);
+        }
+
+        [TestMethod]
+        public void SubTest()
+        {
+            var subCalculator = new Calculator();
+            var res = subCalculator.Sub(20, 10);
+            Assert.AreEqual(res, 10);
+        }
+
+        [TestMethod]
+        public void MulTest()
+        {
+            var mulCalculator = new Calculator();
+            var res = mulCalculator.Mul(20, 10);
+            Assert.AreEqual(res, 200);
+        }
+
+        [TestMethod]
+        public void DivTest()
+        {
+            var divCalculator = new Calculator();
+            var res = divCalculator.Div(20, 10);
+            Assert.AreEqual(res, 2);
+        }
+
+        //TODO
+        [TestMethod]
+        public void DivTest_DivideByZeroError()
+        {
+            var divCalculator = new Calculator();
+            var res = divCalculator.Div(20, 10);
+            Assert.AreEqual(res, 2);
+        }
+    }
+}
